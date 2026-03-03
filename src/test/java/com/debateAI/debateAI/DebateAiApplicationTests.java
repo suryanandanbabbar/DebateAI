5cb46503918a433b02b8c6491e133f2e1882e8ea
Suryanandan Babbar <140876588+suryanandanbabbar@users.noreply.github.com>
2026-03-03 16:08:47 +0530

    Basic Setup

---
+++ src/test/java/com/debateAI/debateAI/DebateAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debateAI.debateAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebateAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
